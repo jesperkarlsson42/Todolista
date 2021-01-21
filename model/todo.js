2409cf75e50e092445e2c0420fb7442a83bd95ad
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2021-01-21 15:33:01 +0100

    added delete btn

--- model/todo.js
+++ model/todo.js
@@ -12,4 +12,5 @@ const todoSchema = new mongoose.Schema({
         }
 })
 
-module.exports = mongoose.model('todo', todoSchema);
+const Todo = mongoose.model("todo", todoSchema);
+module.exports = Todo;
