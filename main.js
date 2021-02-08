485934b0fb2f792175399b5356f1f12c53100878
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2021-02-08 09:36:20 +0100

    Fixed edit with pagination

--- main.js
+++ main.js
@@ -32,6 +32,8 @@ app.get("/", async (req, res) => {
           dataPerPage,
           totalDataPart,
           dataToShow,
+          page,
+          sorted
         });
       } else {
         const data = await Todo.find()
@@ -44,6 +46,8 @@ app.get("/", async (req, res) => {
           dataPerPage,
           totalDataPart,
           dataToShow,
+          page,
+          sorted
         });
       }
     } catch (err) {
@@ -83,21 +87,72 @@ app.get("/delete/:id", async (req, res) => {
 });
 
 app.get("/edit/:id", async (req, res) => {
+  const sorted = +req.query.sorted || 1;
+  const page = +req.query.page || 1;
+  const totalData = await Todo.find().countDocuments();
+  let a = totalData / 10;
+  let x = Math.ceil(a);
   const id = req.params.id;
-  await Todo.find({}, (err, data) => {
-    res.render("editTask.ejs", { todos: data, taskId: id });
-  });
+  if (x < page) {
+    res.redirect("/?page=1");
+  } else {
+    try {
+      let dataPerPage = 10;
+      const totalDataPart = Math.ceil(totalData / dataPerPage);
+      dataToShow = dataPerPage * page;
+      if (page === 1) {
+        const data = await Todo.find()
+          .limit(10)
+          .sort({ date: sorted })
+          .exec(function (err, data) {
+            res.render("editTask.ejs", {
+              page,
+              todos: data,
+              totalData,
+              dataPerPage,
+              totalDataPart,
+              dataToShow,
+              taskId: id,
+              sorted
+            });
+          });
+      } else {
+        const data = await Todo.find()
+          .limit(10)
+          .sort({ date: sorted })
+          .skip((page - 1) * 10)
+          .exec(function (err, data) {
+            res.render("editTask.ejs", {
+              page,
+              todos: data,
+              totalData,
+              dataPerPage,
+              totalDataPart,
+              dataToShow,
+              taskId: id,
+              sorted
+            });
+          });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
+
+
 
 app.post("/edit/:id", async (req, res) => {
   const id = req.params.id;
+  const sorted = +req.query.sorted || 1;
+  let page = +req.query.page || 1;
   await Todo.findByIdAndUpdate(id, { task: req.body.task }, (err) => {
     if (err) {
       console.log("Error Edit task");
       res.send(500, err);
       return;
     } else {
-      res.redirect("/");
+      res.redirect("/?page=" + page + "&&sorted" + sorted);
     }
   });
 });
